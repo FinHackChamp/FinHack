3d26422ed6f3b10267305f6b622799f0bf314cd4
lencialam <lencialam@gmail.com>
2017-07-08 13:25:30 +0800

    Merge branch 'master' into lencia
    # Conflicts:
    #	templates/index.html
    #	templates/js/todo.js

---
+++ serverio.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+from flask import Flask, render_template,request
+from flask_socketio import SocketIO
+import transaction_retriever
+from werkzeug import secure_filename
+from OCR import *
+from transaction_retriever import *
+import sys
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+from flask import request
+from flask_socketio import send, emit
+from os import environ
+import re
+
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+@socketio.on('message')
+def handle_message(message):
+	print(message, file=sys.stderr)
+	send(message)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+    send(json, json=True)
+
+
+@socketio.on_error()        # Handles the default namespace
+def error_handler(e):
+    pass
+@socketio.on_error('/chat') # handles the '/chat' namespace
+def error_handler_chat(e):
+    pass
+@socketio.on_error_default  # handles all namespaces without an explicit error handler
+def default_error_handler(e):
+    pass
+
+@app.route('/api/mode')
+def api_mode():
+	print("request",file = sys.stderr)
+	print (request,file = sys.stderr)
+	mode = request.args.get('mode')
+	print (mode, file = sys.stderr)
+
+	socketio.emit('api_mode',{'mode': mode})
+	return render_template("hotel.html")
+
+@app.route('/api/facility')
+def api_facility():
+	print("request",file = sys.stderr)
+	print (request,file = sys.stderr)
+	action = request.args.get('action')
+	facility = request.args.get('facility')
+	print (action, file = sys.stderr)
+
+	socketio.emit('api_facility',{'action': action, 'facility' : facility})
+	return render_template("hotel.html")
+@socketio.on('connect')
+def connect():
+	@app.route('/api/OCR', methods = ['POST'])
+	def OCR():
+	
+		f = request.files['image']
+		f.save(secure_filename('receipt.jpg'))
+		print (ocr_space_file(filename='receipt.jpg').split('\r\n'), file=sys.stderr)
+		# print (request.files['image'], file= sys.stderr)
+		floatList = []
+		for word in ocr_space_file(filename='receipt.jpg').split('\r\n'):
+			if is_number(word):
+				floatList.append(float(word))
+			print (word)
+		total = max(floatList)
+		print (total	)	
+		socketio.emit('receipt', {'total': total})
+		return render_template("index.html")
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+	HOST = environ.get('SERVER_HOST', 'localhost')
+	try:
+		PORT = int(environ.get('PORT','5009'))
+	except ValueError:
+		PORT = 5009
+
+	print('server running on ' + str(PORT), file=sys.stderr)
+
+	socketio.run(app, port =  PORT, host= '0.0.0.0')
+
---
+++ transaction_retriever.py
@@ -0,0 +1,102 @@
+import numpy as numpy
+import pandas as pd
+
+df = pd.read_csv('transaction.csv')
+labels = set(df['company label'])
+salary_segmentation = [10000,50000,100000,500000,1000000]
+
+
+
+
+# return personal info for each category
+# [{'amount': 922.05999999999995, 'drilldown': 'Grocery', 'name': 'Grocery'}, {...}]
+def getPersonalAnalysis(name):
+    
+    personal = df.loc[df['name'] == name, ['company name', 'company label', 'amount']]
+    output = []
+    for label in labels:
+        amount = sum(personal.loc[personal['company label'] == label,'amount'])
+        output.append({'name': label, 'amount': amount, 'drilldown':label})
+    return output
+# getPersonalAnalysis('Rachel Trujillo')
+
+
+# Intput: 
+# criteria: a list ['gender', 'age','salary']
+# target: if == 'category', only return average spending for each category
+# 
+# return: [{'amount': 154.95960794655397, 'drilldown': 'Grocery', 'name': 'Grocery'}, {...}]
+#		  if == 'percentage', return total number, percentage and index in the group, and the list of amount of spending for
+#                each people in each category
+# return:{'total_people': 500, education': {'index': 400, 'amount_list': [...], 'percentage': 0.8}}
+def getComparison(name, criteria , target='category'):
+    personal = df.loc[df['name'] == name, ['age','salary','gender','company label', 'amount']]
+    age = personal.iloc[0]['age']
+    salary = personal.iloc[0]['salary']
+    gender = personal.iloc[0]['gender']
+    selectedDf = df
+    criteria = set(criteria)
+    if 'gender' in criteria:
+        selectedDf = selectedDf.loc[selectedDf['gender'] == gender]
+    if 'salary' in criteria:
+        lowThresh = None
+        highThresh = None
+        
+        for s in salary_segmentation:
+#             print (s)
+            if salary < s:
+#                 print (s)
+                if s == salary_segmentation[0]:
+                    highThresh = s
+                elif s == salary_segmentation[-1]:
+                    lowThresh = s
+                else:
+                    index = salary_segmentation.index(s)
+                    lowThresh = salary_segmentation[index-1]
+                    highThresh = salary_segmentation[index]
+                break
+        print (lowThresh)
+        print(highThresh)
+        if lowThresh is not None: 
+            print ('fff')
+            selectedDf = selectedDf.loc[selectedDf['salary'] >= lowThresh]
+        if highThresh is not None: 
+            selectedDf = selectedDf.loc[selectedDf['salary'] < highThresh]
+    if 'age' in criteria:
+        lowThresh = age-age%10
+        highThresh = lowThresh+9
+        selectedDf = selectedDf.loc[selectedDf['age'] >= lowThresh]
+        selectedDf = selectedDf.loc[selectedDf['age'] < highThresh]
+    output = []
+    if target =='percentage':
+    	output = {}
+    num_people= len(set(selectedDf.name))
+    for label in labels:
+
+        if target == 'category':
+            amount = sum(selectedDf.loc[selectedDf['company label'] == label,'amount'])/len(selectedDf.loc[selectedDf['company label'] == label,'amount'])
+            output.append({'name': label, 'amount': amount, 'drilldown':label})
+            
+        elif target == 'percentage':
+            
+            output['total_people'] = num_people
+            amountList = []
+            for name in set(selectedDf.name):
+
+                thatPersonDf = selectedDf.loc[selectedDf['name'] == name]
+    #             print(thatPersonDf)
+    #             print (thatPersonDf.loc[thatPersonDf['company label'] == label, 'amount'].dtype)
+                amount = (thatPersonDf.loc[thatPersonDf['company label'] == label, 'amount']).sum()
+                amountList.append(amount)
+            amountList.sort()
+
+            thisPersonAmount = sum(personal.loc[personal['company label'] == label, 'amount'])
+            index = amountList.index(thisPersonAmount)
+            output[label] = {'index':index, 'percentage': (index+0.0)/ num_people, 'amount_list':amountList}
+    print ('loaded')
+    return output
+   
+# print getComparison('Rachel Trujillo', ['gender','age'], target='percentage')
+
+
+
